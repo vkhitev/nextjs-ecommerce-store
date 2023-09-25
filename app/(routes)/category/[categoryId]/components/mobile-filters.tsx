e976924c222b294dd8889960c0c7e99b403cdbe9
Vlad Khitev <vkhitev@axon.dev>
2023-09-25 16:41:55 +0300

    feat: add category page

---
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { useState } from 'react'
+import { PlusIcon, XIcon } from 'lucide-react'
+import { Dialog } from '@headlessui/react'
+
+import { Color, Size } from '@/types'
+import { Button } from '@/components/ui/button'
+import { IconButton } from '@/components/ui/icon-button'
+import { Filter } from './filter'
+
+interface MobileFiltersProps {
+  sizes: Array<Size>
+  colors: Array<Color>
+}
+
+export const MobileFilters = ({ sizes, colors }: MobileFiltersProps) => {
+  const [open, setOpen] = useState(false)
+
+  const onOpen = () => {
+    setOpen(true)
+  }
+
+  const onClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <Button className="flex items-center gap-x-2 lg:hidden" onClick={onOpen}>
+        Filters
+        <PlusIcon size={20} />
+      </Button>
+
+      <Dialog
+        open={open}
+        as="div"
+        className="relative z-40 lg:hidden"
+        onClose={onClose}
+      >
+        {/* Background */}
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+        {/* Dialog position */}
+        <div className="fixed inset-0 z-40 flex">
+          <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+            {/* Close button */}
+            <div className="flex items-center justify-end px-4">
+              <IconButton icon={<XIcon size={15} />} onClick={onClose} />
+            </div>
+
+            {/* Filters */}
+            <div className="p-4">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
+    </>
+  )
+}
