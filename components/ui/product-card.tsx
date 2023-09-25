4ea55af9cd76a1c0ef3c628389ffff419b20a1d7
Vlad Khitev <vkhitev@axon.dev>
2023-09-25 15:56:13 +0300

    feat: add single product page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,14 +6,24 @@ import { ExpandIcon, ShoppingCartIcon } from 'lucide-react'
 import { Product } from '@/types'
 import { IconButton } from '@/components/ui/icon-button'
 import { Currency } from '@/components/ui/currency'
+import { useRouter } from 'next/navigation'
 
 interface ProductCardProps {
   data: Product
 }
 
 export const ProductCard = ({ data }: ProductCardProps) => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push(`/product/${data.id}`)
+  }
+
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       {/* Images anc Actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
