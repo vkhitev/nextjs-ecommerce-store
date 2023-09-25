1fc43ff662460326af3f7404eb86d08efaa17620
Vlad Khitev <vkhitev@axon.dev>
2023-09-25 19:18:02 +0300

    feat: disable checkout on empty cart

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -49,7 +49,11 @@ export const Summary = () => {
           <Currency value={totalPrice.toString()} />
         </div>
       </div>
-      <Button className="w-full mt-6" onClick={onCheckout}>
+      <Button
+        disabled={items.length === 0}
+        className="w-full mt-6"
+        onClick={onCheckout}
+      >
         Checkout
       </Button>
     </div>
